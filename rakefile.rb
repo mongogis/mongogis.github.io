d2209634be53ad05e29b3df7fe64157cd37cd748
huangtao <ht.anglenx@gmail.com>
2014-04-26 17:27:17 +0800

    add init

---
+++ rakefile.rb
@@ -0,0 +1,36 @@
+SOURCE = "."
+CONFIG = {
+  'posts' => File.join(SOURCE, "_posts"),
+  'post_ext' => "md",
+}
+
+desc "Launch preview environment"
+task :preview do
+  sh "jekyll serve -w"
+end # task :preview
+
+desc "Begin a new post in #{CONFIG['posts']}"
+task :post do
+  abort("rake aborted: '#{CONFIG['posts']}' directory not found.") unless FileTest.directory?(CONFIG['posts'])
+  title = ENV["title"] || "new-post"
+  slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+
+  begin
+    date = (ENV['date'] ? Time.parse(ENV['date']) : Time.now).strftime('%Y-%m-%d')
+  rescue => e
+    puts "Error - date format must be YYYY-MM-DD, please check you typed it correctly!"
+    exit -1
+  end
+  filename = File.join(CONFIG['posts'], "#{date}-#{slug}.#{CONFIG['post_ext']}")
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  
+  puts "Creating new post: #{filename}"
+  open(filename, 'w') do |post|
+    post.puts "---"
+    post.puts "layout: post"
+    post.puts "title: \"#{title.gsub(/-/,' ')}\""
+    post.puts "---"
+  end
+end # task :post
